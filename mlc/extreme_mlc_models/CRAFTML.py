1386e49d9a25f18e101b9209d86cf4d4c35a31f6
Kosuke Yoshimura <kosuke.yoshimura@icloud.com>
2018-07-22 18:47:13 +0900

    add test code of CLAFTML.py

--- mlc/extreme_mlc_models/CRAFTML.py
+++ mlc/extreme_mlc_models/CRAFTML.py
@@ -1,9 +1,7 @@
 """
-Method Name: CRAFTML (Clustering-based RAndom Forest of predictive Trees for
-extreme Multi-label Learning)
+Method Name: CRAFTML (Clustering-based RAndom Forest of predictive Trees for extreme Multi-label Learning)
 
-Title: CRAFTML, an Efficient Clustering-based Random
-Forest for Extreme Multi-label Learning
+Title: CRAFTML, an Efficient Clustering-based Random Forest for Extreme Multi-label Learning
 
 URL: http://proceedings.mlr.press/v80/siblini18a.html
 """
---
+++ mlc/extreme_mlc_models/tests/test_CRAFTML.py
@@ -0,0 +1,6 @@
+import unittest
+
+class TestCRAFTML(unittest.TestCase):
+    """test code of CRAFTML.py"""
+    def test_train_node_classifier(self):
+        pass
