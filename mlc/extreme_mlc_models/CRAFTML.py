73721a79c83d354c2c23f420663aa11dde8dcdbf
Kosuke Yoshimura <kosuke.yoshimura@icloud.com>
2018-07-22 18:28:13 +0900

    add the info of CRAFTML.

---
+++ mlc/extreme_mlc_models/CRAFTML.py
@@ -0,0 +1,9 @@
+"""
+Method Name: CRAFTML (Clustering-based RAndom Forest of predictive Trees for
+extreme Multi-label Learning)
+
+Title: CRAFTML, an Efficient Clustering-based Random
+Forest for Extreme Multi-label Learning
+
+URL: http://proceedings.mlr.press/v80/siblini18a.html
+"""
